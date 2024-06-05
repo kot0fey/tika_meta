4571cb9e6a5eba724e3071b93e0524d9a456cb41
prutsk <prutsk@mail.ru>
2024-06-05 13:55:56 +0300

    Ingest plugin

--- src/main/java/org/example/ingest/ExtractMetadataProcessor.java
+++ src/main/java/org/example/ingest/ExtractMetadataProcessor.java
@@ -1,10 +1,17 @@
 package org.example.ingest;
 
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
 import org.elasticsearch.ingest.AbstractProcessor;
 import org.elasticsearch.ingest.IngestDocument;
 import org.elasticsearch.ingest.Processor;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 import static org.elasticsearch.ingest.ConfigurationUtils.readStringProperty;
@@ -24,11 +31,28 @@ public class ExtractMetadataProcessor extends AbstractProcessor {
 
     @Override
     public IngestDocument execute(IngestDocument ingestDocument) {
-        String content = ingestDocument.getFieldValue(field, String.class);
-        String metadata = MetadataExtractor.extract(content);
-        ingestDocument.setFieldValue(targetField, metadata);
-        return ingestDocument;
+        try (InputStream stream = ingestDocument.getFieldValue(field, InputStream.class)) {
+            Metadata metadata = new Metadata();
+            AutoDetectParser parser = new AutoDetectParser();
+            BodyContentHandler handler = new BodyContentHandler();
+            ParseContext context = new ParseContext();
+            parser.parse(stream, handler, metadata, context);
+
+            ingestDocument.setFieldValue(targetField, metadata);
+            for (String name : metadata.names()) {
+                ingestDocument.setFieldValue(targetField + "." + name, metadata.get(name));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (TikaException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        } finally {
+            return ingestDocument;
+        }
     }
+
 
     @Override
     public String getType() {
